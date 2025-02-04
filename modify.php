1ecc1557d50a075be937e84e9afec22de648fe8a
wookyung nam <namwookyung.dev@gmail.com>
2025-02-04 17:59:50 +0900

    글 수정 화면 수정 및 api 추가가

--- modify.php
+++ modify.php
@@ -1,7 +1,7 @@
 <?php
     include $_SERVER['DOCUMENT_ROOT']."/php_board/db.php";
-    $idx = $_GET['idx'];
-    $sql = query("SELECT * FROM php_board WHERE idx='$idx';");
+    $post_no = $_GET['idx'];
+    $sql = query("SELECT * FROM php_board WHERE idx='$post_no';");
     $board = $sql -> fetch_array();
 ?>
 <!doctype html>
@@ -17,9 +17,23 @@
             </h1>
             <h4>글을 수정합니다.</h4>
             <div id="write_area">
-                <form action="modify_ok.php?idx=<?php echo $idx; ?>" method="post">
+                <form action="modify_ok.php?idx=<?php echo $post_no; ?>" method="post">
                     <div id="in_title">
-                        <textarea name="title" id="utitle" rows="1" cols="55" placeholder="글쓴이" maxlength="100" required>
-                            <?php echo $board['name']; ?>
-                        </textarea>
+                        <textarea name="post_title" id="utitle" rows="1" cols="55" placeholder="제목목" maxlength="100" required><?php echo $board['post_title']; ?></textarea>
                     </div>
+                    <hr />
+                    <div id="in_name">
+                        <textarea name="user_name" id="user_name" rows="1" cols="55" placeholder="글쓴이" maxlength="100" required><?php echo $board['user_name']; ?></textarea>
+                    </div>
+                    <hr />
+                    <div id="in_content">
+                        <textarea name="post_content" id="post_content" placeholder="내용" required><?php echo $board['post_content']; ?></textarea>
+                    </div>
+                    <div class="bt_se">
+                        <button type="submit">수정 완료</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ modify_ok.php
@@ -0,0 +1,13 @@
+<?php
+    include $_SERVER['DOCUMENT_ROOT'].'/php_board/db.php';
+
+    $post_no = $_GET['idx'];
+    $user_name = $_POST['user_name'];
+    $user_pw = password_hash($_POST['user_pw'], PASSWORD_DEFAULT);
+    $post_title = $_POST['post_title'];
+    $post_content = $_POST['post_content'];
+    $sql = query("UPDATE php_board SET user_name = '".$user_name."', user_pw = '".$user_pw."', post_title = '".$post_title."',
+        post_content = '".$post_content."' WHERE idx = '".$post_no."'"); ?>
+
+    <script type="text/javascript">alert("수정 완료");</script>
+    <meta http-equiv="refresh" content="0; url=/php_board/index.php">   <!-- 0초 후에 url 주소로 리다이렉션 -->
